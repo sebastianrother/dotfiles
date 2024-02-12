0fed7316b50077d2a73347c09c8c68e5b1647a09
sebastianrother <basti@sebastianrother.com>
2024-02-12 19:43:19 +0100

    adding dotfiles

---
+++ neovim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,36 @@
+vim.g.editorconfig = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.termguicolors = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.wrap = false
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↵")
+
+-- Auto-complete commands to longest string, then open menu.
+vim.opt.wildmode = "longest,full"
+
+vim.opt.clipboard:append("unnamedplus")
