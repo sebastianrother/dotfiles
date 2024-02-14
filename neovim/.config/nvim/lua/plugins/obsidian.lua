a10099a6aba41e22a74c876c9c50980ec805863b
sebastianrother <sebastian.rother@gmx.net>
2024-02-14 08:24:49 +0000

    adding obsidan support

---
+++ neovim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,32 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "digital_brain",
+        path = "~/Documents/digital_brain",
+      },
+    },
+
+    daily_notes = {
+      folder = "02_areas/work/daily_notes",
+      template = nil,
+    },
+
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+      new_notes_location = "notes_subdir",
+      preferred_link_style = "wiki",
+      prepend_note_id = false,
+      prepend_note_path = false,
+      use_path_only = true,
+    },
+
+    disable_frontmatter = true,
+  },
+}
