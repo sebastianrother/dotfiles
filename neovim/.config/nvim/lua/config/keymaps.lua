bd0cb764c86f98feade75dafb3d496ae3fc5dabc
sebastianrother <sebastian.rother@gmx.net>
2024-02-26 10:38:30 +0000

    feat: dont yank when doing visiual replace

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,6 @@ vim.keymap.set("n", "<leader>x", ":bw<CR>")
 -- Recenter screen after going up and down half a page
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Visual replace without yanking
+vim.keymap.set("v", "p", '"_dP')
