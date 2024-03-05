1a49703a306f1eb273792dcc6772b6139fa03ea0
sebastianrother <sebastian.rother@gmx.net>
2024-03-05 17:21:16 +0000

    feat: add prettier to tsx

--- neovim/.config/nvim/lua/plugins/null-ls.lua
+++ neovim/.config/nvim/lua/plugins/null-ls.lua
@@ -12,7 +12,7 @@ return {
       }),
       formatting.prettier.with({
         prefer_local = "node_modules/.bin",
-        extra_filetypes = { "astro" },
+        extra_filetypes = { "astro", "tsx" },
       }),
       formatting.black.with({
         only_local = "venv/bin",
