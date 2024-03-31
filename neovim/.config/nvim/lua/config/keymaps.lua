62b0cebd4b3a490a56b4fbab0ca1e503153dedb7
sebastianrother <sebastian.rother@gmx.net>
2024-03-31 08:58:26 +0100

    feat: support renaming

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -14,6 +14,7 @@ vim.keymap.set("n", "<leader>x", ":bw<CR>")
 -- Recenter screen after going up and down half a page
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "rr", ":lua vim.lsp.buf.rename()<CR>")
 
 -- Visual replace without yanking
 vim.keymap.set("v", "p", '"_dP')
