e11910a78cef0faeab286afaaea13c6806645efa
sebastianrother <sebastian.rother@gmx.net>
2025-05-11 06:36:37 +0100

    feat: half baked migration to neovim 0.11.1

--- neovim/.config/nvim/lua/plugins/core.lua
+++ neovim/.config/nvim/lua/plugins/core.lua
@@ -1,5 +1,4 @@
 return {
   "tpope/vim-fugitive",
   "airblade/vim-gitgutter",
-  "github/copilot.vim",
 }
--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,127 @@
-return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup.
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
+vim.lsp.config("pylsp", {
+  cmd = (function()
+    local cwd = vim.fn.getcwd()
+    local cwd_path_parts = vim.split(cwd, "/", { trimempty = true })
+    local base_dir = cwd_path_parts[#cwd_path_parts]
+    local dockerfile = "~/.config/nvim/docker-lsp/" .. base_dir .. "/Dockerfile"
+
+    if vim.fn.filereadable(vim.fn.expand(dockerfile)) == 1 then
+      return {
+        "docker",
+        "run",
+        "-i",
+        "--name",
+        "lsp-" .. base_dir,
+        "--rm",
+        "--workdir=" .. cwd,
+        "--volume=" .. cwd .. ":" .. cwd,
+        "lsp/" .. base_dir,
+      }
+    else
+      return nil
+    end
+  end)(),
+  settings = {
+    pylsp = {
+      plugins = {
+        black = {
+          enabled = true,
+        },
+        flake8 = {
+          enabled = true,
+        },
+        mccabe = {
+          enabled = false,
+        },
+        pycodestyle = {
+          enabled = false,
+        },
+        pyflakes = {
+          enabled = false,
+        },
+        pylsp_mypy = {
+          enabled = false,
+          dmypy = true,
+          report_progress = true,
+          live_mode = false,
+        },
+        pylint = {
+          enabled = true,
+        },
+        rope_completion = {
+          enabled = true,
+        },
+      },
+    }
   },
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    '.git',
+  },
+})
+
+vim.lsp.enable("pylsp")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("astro")
+vim.lsp.enable("tailwindcss")
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc)
+      vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
+    end
+
+    map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+    map("gh", vim.lsp.buf.hover, "[H]over")
+
+    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    map("<leader>rr", vim.lsp.buf.rename, "Rename symbol")
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    -- Disable tsserver formatting as it conflicts with prettier.
+    if client.name == "ts_ls" then
+      client.server_capabilities.documentFormattingProvider = false
+    end
+    --
+    -- Format on save.
+    if client and client:supports_method("textDocument/formatting") then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = event.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = event.buf, id = client.id })
+        end,
+      })
+    end
+
+    -- Highlight references on hover.
+    if client and client.server_capabilities.documentHighlightProvider then
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        buffer = event.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
+  end
+})
+
+return {
   {
     "williamboman/mason.nvim",
     lazy = false,
@@ -23,136 +135,21 @@ return {
     dependencies = {
       { "L3MON4D3/LuaSnip" },
       { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-buffer" },
     },
     config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_cmp()
-
       local cmp = require("cmp")
-      local cmp_action = lsp_zero.cmp_action()
 
       cmp.setup({
-        formatting = lsp_zero.cmp_format(),
+        completion = { completeopt = "menu,menuone,noinsert" },
         mapping = cmp.mapping.preset.insert({
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-u>"] = cmp.mapping.scroll_docs(-4),
           ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
         }),
-      })
-    end,
-  },
-
-  -- LSP.
-  {
-    "neovim/nvim-lspconfig",
-    cmd = { "LspInfo", "LspInstall", "LspStart" },
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "williamboman/mason-lspconfig.nvim" },
-    },
-    config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig()
-
-      lsp_zero.on_attach(function(client, bufnr)
-        -- Keybindings.
-        lsp_zero.default_keymaps({ buffer = bufnr })
-
-        vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "ds", "<cmd>Telescope lsp_document_symbols<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = bufnr })
-
-        -- Disable tsserver formatting as it conflicts with prettier.
-        if client.name == "ts_ls" then
-          client.server_capabilities.documentFormattingProvider = false
-        end
-
-
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end)
-
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "ts_ls",
-          "lua_ls",
-          "pylsp",
-          "tailwindcss",
-          "astro",
-          "gopls",
-        },
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require("lspconfig").lua_ls.setup(lua_opts)
-          end,
-          pylsp = function()
-            require("lspconfig").pylsp.setup({
-              cmd = (function()
-                local cwd = vim.fn.getcwd()
-                local cwd_path_parts = vim.split(cwd, "/", { trimempty = true })
-                local base_dir = cwd_path_parts[#cwd_path_parts]
-                local dockerfile = "~/.config/nvim/docker-lsp/" .. base_dir .. "/Dockerfile"
-
-                if vim.fn.filereadable(vim.fn.expand(dockerfile)) == 1 then
-                  return {
-                    "docker",
-                    "run",
-                    "-i",
-                    "--name",
-                    "lsp-" .. base_dir,
-                    "--rm",
-                    "--workdir=" .. cwd,
-                    "--volume=" .. cwd .. ":" .. cwd,
-                    "lsp/" .. base_dir,
-                  }
-                else
-                  return nil
-                end
-              end)(),
-              plugins = {
-                black = {
-                  enabled = true,
-                },
-                flake8 = {
-                  enabled = true,
-                },
-                mccabe = {
-                  enabled = false,
-                },
-                pycodestyle = {
-                  enabled = false,
-                },
-                pyflakes = {
-                  enabled = false,
-                },
-                pylsp_mypy = {
-                  enabled = false,
-                  dmypy = true,
-                  report_progress = true,
-                  live_mode = false,
-                },
-                pylint = {
-                  enabled = true,
-                },
-                rope_completion = {
-                  enabled = true,
-                },
-              },
-            })
-          end,
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "path" },
         },
       })
     end,
